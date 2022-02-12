0632e71c18a310ba698755a92bacaea17ed9e947
samuel <samuelsato2009@gmail.com>
2022-02-12 01:20:52 -0300

    deleteById

--- src/main/java/one/digitalInovation/personAPI/service/PersonService.java
+++ src/main/java/one/digitalInovation/personAPI/service/PersonService.java
@@ -47,12 +47,21 @@ public class PersonService {
     }
 
 
-    public PersonDTO findById(@PathVariable Long id ) throws PersonNotFoundException {
+    public PersonDTO findById(Long id ) throws PersonNotFoundException {
         //Optional<Person> optionPerson =  personRepository.findById(id);
-        Person person = personRepository.findById(id).orElseThrow(() -> new PersonNotFoundException(id));
+        Person person = verifyIfExists(id);
 
          return personMapper.toDTO(person);
     }
 
 
+    public void delete(Long id) throws PersonNotFoundException {
+        verifyIfExists(id);
+
+        personRepository.deleteById(id);
+    }
+
+    private Person verifyIfExists(Long id) throws PersonNotFoundException {
+        return personRepository.findById(id).orElseThrow(() -> new PersonNotFoundException(id));
+    }
 }
